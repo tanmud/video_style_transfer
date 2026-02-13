d877962c4c545949897b82f4197d400e9a5ecac6
Tanish Mudaliar <180313086+tanmud@users.noreply.github.com>
2026-02-13 17:10:52 -0500

    fixed video path

--- train_animatediff.sh
+++ train_animatediff.sh
@@ -10,7 +10,7 @@ export WANDB_MODE="offline"
 # Video Data Settings
 export NUM_FRAMES=16  # Number of frames to sample per video
 export RESOLUTION=512  # Lower resolution for video (memory)
-export INSTANCE_DIR="male_biker/male_biker"
+export INSTANCE_DIR="./instance_videos/male_biker"
 export OUTPUT_DIR="models/male_biker/male_biker"
 export STEPS=10000
 
